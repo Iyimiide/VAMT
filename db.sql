0e65a1a37a693908af9f91cb5b751f022200e909
Iyimiide <124675039+Iyimiide@users.noreply.github.com>
2023-10-01 22:53:29 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,41 +2,29 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
---
--- Database: `db`
---
+-- Specify the database context
+USE db;
 
--- --------------------------------------------------------
+-- Create a table to store gallery images
+CREATE TABLE gallery_images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_path VARCHAR(255) NOT NULL,
+    description TEXT
+);
 
---
--- Table structure for table `tb_user`
---
+-- Create a table to store slideshow images
+CREATE TABLE slideshow_images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_path VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE `tb_user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(75) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
 -- Indexes for dumped tables
---
-
---
--- Indexes for table `tb_user`
---
-ALTER TABLE `tb_user`
+ALTER TABLE `gallery_images`
   ADD PRIMARY KEY (`id`);
 
---
 -- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tb_user`
---
-ALTER TABLE `tb_user`
+ALTER TABLE `gallery_images`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+-- Commit the transaction
 COMMIT;
