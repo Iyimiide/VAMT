22589e06e57237efda0add6788f239974918373b
Iyimiide <124675039+Iyimiide@users.noreply.github.com>
2023-10-02 11:50:18 +0100

    Update landing.php

--- landing.php
+++ landing.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <!-- Include Swiper CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <style>
-      /* Style for the logo container */
-        .logo-container {
+.logo-container {
             display: flex;
             align-items: center; /* Center the logo vertically */
             padding: 10px 20px; /* Add padding around the logo */
@@ -17,27 +18,7 @@
             height: auto; /* Automatically adjust the height while maintaining aspect ratio */
             margin-right: 20px; /* Add spacing between logo and navigation items */
         }
-
-        /* Reset some default styling */
-        body, h1, h2, p, ul, li {
-            margin: 0;
-            padding: 0;
-        }
-
-        /* Basic styling */
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
-            animation: colorChange 10s infinite alternate;
-        }
-
-        @keyframes colorChange {
-            0% { background-color: #f4e7d2; } /* Warm beige */
-            50% { background-color: #c85a54; } /* Wine red */
-            100% { background-color: #f4e7d2; } /* Back to warm beige */
-        }
-
-   header {
+header {
     background-color: white; /* Change the background color to white */
     color: darkgoldenrod; /* Change the font color to dark gold */
     padding: 10px 0;
@@ -115,158 +96,6 @@ ul.dropdown-menu a:hover {
         .redirect-link a:hover {
             background-color: #ff3366;
         }
-/* Responsive styles */
-@media screen and (max-width: 768px) {
-    /* Slideshow container styles */
-   .slideshow-container {
-        max-width: 100%;
-    }
-        position: relative;
-            max-width: 1000px;
-            margin: auto;
-        }
-
-        .slide {
-            display: none;
-            text-align: center;
-        }
-
-        img {
-            max-width: 50%;
-            height: auto;
-            display: block;
-            margin: auto;
-        }
-
-        .prev, .next {
-            cursor: pointer;
-            position: absolute;
-            top: 50%;
-            transform: translateY(-50%);
-            font-size: 18px;
-        }
-
-        .next {
-            right: 10px;
-        }
-
-        .prev {
-            left: 10px;
-        }
-
-
-        .redirect-link {
-            text-align: center;
-            margin-top: 20px;
-        }
-
-        .redirect-link a {
-            background-color: #66ccff;
-            color: white;
-            padding: 10px 20px;
-            border-radius: 5px;
-            text-decoration: none;
-        }
-
-        .areas-of-work {
-            padding: 40px;
-            text-align: center;
-            color: #333;
-        }
-
-        .work-area {
-            position: relative;
-        }
-
-        .work-area h3 {
-            cursor: pointer;
-        }
-
-        .work-area p {
-            display: none;
-            padding: 10px;
-            position: absolute;
-            top: 100%;
-            left: 0;
-            width: 100%;
-            background-color: white;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            border-radius: 5px;
-            z-index: 1;
-        }
-
-        .work-area:hover p {
-            display: block;
-        }
-
-        .foundation-events {
-            padding: 40px;
-            text-align: center;
-            color: #333;
-            background-color: #f9f9f9;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        }
-
-        .event {
-            margin: 20px 0;
-            padding: 20px;
-            background-color: white;
-            border-radius: 10px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        }
-
-        .event h3 {
-            font-size: 20px;
-            margin-bottom: 10px;
-        }
-
-        .event p {
-            font-size: 14px;
-            margin-bottom: 8px;
-        }
-
-        .event-description {
-            font-size: 14px;
-            color: #777;
-        }
-        /* Styles for the Associations We Partner With section */
-.associations {
-    background-color: white;
-    padding: 40px 0;
-    text-align: center;
-}
-
-.associations h2 {
-    font-size: 24px;
-    margin-bottom: 20px;
-}
-
-.association-logo {
-    display: inline-block;
-    margin: 0 20px; /* Adjust spacing between logos */
-}
-
-.association-logo img {
-    max-width: 100px; /* Adjust the maximum width of the logos */
-    height: auto;
-}
-
-        .foundation-stats {
-            background-color: white;
-            display: grid;
-            grid-template-columns: repeat(3, 1fr);
-            gap: 20px;
-            font-weight: bold;
-        }
-
-        .foundation-stats div {
-            padding: 20px;
-            border: 1px solid #e0e0e0;
-            border-radius: 10px;
-        }
-
-        /* Responsive styles */
          /* Footer styles */
         footer {
             background-color: #333;
